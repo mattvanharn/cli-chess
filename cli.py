56a63da6adcf52f28e36c99b9268b949b168b633
Matt Van Harn <mattvanharn@gmail.com>
2023-01-18 09:27:23 -0500

    Initialize game.py and cli.py

---
+++ cli.py
@@ -0,0 +1,6 @@
+"""
+This module implements a command-line interface for the Chess player.
+
+@author: Matt Van Harn
+"""
+from game import Chess
---
+++ game.py
@@ -0,0 +1,9 @@
+"""
+A Chess application.
+Runs a game of chess for the command-line interface.
+
+@author: Matt Van Harn
+"""
+
+
+class Chess:
